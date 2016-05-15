50e6942810043324fada84315dc321927cfdb8c2
quadrohedron <quadrohedron@gmail.com>
2016-05-15 19:01:20 +0100

    Create igyroui.py

---
+++ igyroui.py
@@ -0,0 +1,114 @@
+[
+  {
+    "selected" : false,
+    "frame" : "{{0, 0}, {640, 480}}",
+    "class" : "View",
+    "nodes" : [
+      {
+        "selected" : false,
+        "frame" : "{{0, 0}, {640, 480}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "frame" : "{{270, 190}, {100, 100}}",
+          "background_color" : "RGBA(0.000000,0.000000,0.000000,1.000000)",
+          "class" : "View",
+          "name" : "view5",
+          "uuid" : "0E82042F-CA49-4628-97B7-DA7A55BA599E"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{480, 0}, {160, 450}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "custom_attributes" : "{'scene':abars()}",
+          "frame" : "{{270, 190}, {100, 100}}",
+          "uuid" : "63D318D2-2872-4CF4-88FE-1258ED37680F",
+          "class" : "View",
+          "name" : "view2",
+          "custom_class" : "SceneView"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{0, 0}, {160, 450}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "frame" : "{{270, 190}, {100, 100}}",
+          "class" : "View",
+          "name" : "view1",
+          "uuid" : "35D3DB21-229E-4775-A8C3-1FCB6C54D765"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{160, 0}, {160, 450}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "frame" : "{{270, 190}, {100, 100}}",
+          "class" : "View",
+          "name" : "view3",
+          "uuid" : "79163F3C-5F3C-45E8-A7EC-68524F7D92CD"
+        }
+      },
+      {
+        "selected" : true,
+        "frame" : "{{320, 0}, {160, 450}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "view4",
+          "frame" : "{{270, 190}, {100, 100}}",
+          "uuid" : "CF45A63A-FE0C-4B36-A6E9-E4C766E87676",
+          "custom_class" : "SceneView",
+          "custom_attributes" : "{'scene':vbars()}",
+          "class" : "View"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{160, 450}, {320, 30}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "border_width" : 2,
+          "action" : "rezero",
+          "frame" : "{{280, 224}, {80, 32}}",
+          "title" : "Z E R O",
+          "class" : "Button",
+          "font_bold" : true,
+          "uuid" : "8A55A838-7C93-4EC0-B85D-B3D3BEE74471",
+          "corner_radius" : 5,
+          "border_color" : "RGBA(1.000000,1.000000,1.000000,1.000000)",
+          "font_size" : 20,
+          "name" : "button1"
+        }
+      }
+    ],
+    "attributes" : {
+      "flex" : "",
+      "enabled" : true,
+      "tint_color" : "RGBA(0.000000,0.478000,1.000000,1.000000)",
+      "custom_class" : "",
+      "border_color" : "RGBA(0.000000,0.000000,0.000000,1.000000)",
+      "background_color" : "RGBA(1.000000,1.000000,1.000000,1.000000)",
+      "name" : "movement"
+    }
+  }
+]
