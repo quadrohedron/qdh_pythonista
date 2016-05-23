e69d6abd1e30e25d02dee18e5817267dfa85079c
quadrohedron <quadrohedron@gmail.com>
2016-05-23 19:25:53 +0100

    Create xplodrrrui.py

---
+++ xplodrrrui.py
@@ -0,0 +1,679 @@
+[
+  {
+    "selected" : false,
+    "frame" : "{{0, 0}, {1024, 740}}",
+    "class" : "View",
+    "nodes" : [
+      {
+        "selected" : false,
+        "frame" : "{{0, 0}, {724, 704}}",
+        "class" : "View",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "custom_attributes" : "{'scene':xploder()}",
+          "frame" : "{{462, 334}, {100, 100}}",
+          "class" : "View",
+          "name" : "mainscene",
+          "custom_class" : "scene.SceneView",
+          "uuid" : "522CEE96-0553-40B9-A1FD-E86706913F60"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 20}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label1",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "68AE0A56-7D36-4F75-BA44-B62E57F48CCE",
+          "text" : "G0.x",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 20}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "Gx",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "36796360-851E-43EE-9E1A-D85B11745715",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "362",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 60}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label2",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "17D58FEF-4460-403F-96D9-27070D0DD86F",
+          "text" : "G0.y",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 60}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "Gy",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "E51FD1D9-AFC3-496E-B532-5CD62182F6FB",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "352",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 100}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label3",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "F9F138CB-84AE-4FBD-8533-5B13118781A5",
+          "text" : "Vμ",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 100}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "vmu",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "BA652EB0-30B4-4E9C-BB19-E1C2F9218F5A",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "1",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 140}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label4",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "33CF242F-2CA3-494A-86A7-76C18CA7532A",
+          "text" : "σVμ",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 140}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "sigvmu",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "C0DFC791-C149-4CF4-8CBA-E4821C14F506",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "0",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 180}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label5",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "73B78B4E-C551-436E-8E86-6D28013E77F1",
+          "text" : "θμ",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 180}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "0AE751C0-4C01-4337-926D-69676B7C7871",
+          "secure" : false,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "custom_attributes" : "{'action':tfee}",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "-1",
+          "placeholder" : "(in degrees, neg for none)",
+          "font_name" : "<System>",
+          "spellchecking_type" : "no",
+          "class" : "TextField",
+          "name" : "thmu",
+          "font_size" : 17
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 220}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label6",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "28133950-4056-4884-AA24-7C12F118F61D",
+          "text" : "σθμ",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 220}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "sigthmu",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "8F12DF77-E1AD-4DD6-B6D7-D302408AD189",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "0",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{725, 300}, {230, 31}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label7",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "12AE837F-168D-46A3-8530-39017DEA3FE3",
+          "text" : "Bounded",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{968, 300}, {51, 31}}",
+        "class" : "Switch",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "action" : "cswitch",
+          "frame" : "{{487, 369}, {51, 31}}",
+          "value" : true,
+          "class" : "Switch",
+          "name" : "boundsw",
+          "uuid" : "464DEDBF-028B-40CE-9EC2-A92A6E58963F"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 339}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label8",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "1492F5DA-FCE5-419A-B433-95413AB488FE",
+          "text" : "COR",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 260}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label9",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "74CA1623-EB5D-41FA-8545-4679D0F1CC5C",
+          "text" : "g",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 260}, {98, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "g",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "54B45BBE-2908-433E-A15E-1F53A1239B86",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "text" : "9.81",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{908, 260}, {52, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "border_width" : 0,
+          "action" : "setng",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "9.81",
+          "class" : "Button",
+          "uuid" : "70B65810-AFD6-4811-95CB-ABA08010AAD1",
+          "corner_radius" : 0,
+          "font_size" : 15,
+          "name" : "button1"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 339}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "cor",
+          "font_size" : 17,
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "class" : "TextField",
+          "text" : "0",
+          "uuid" : "2B13D578-1B27-4A86-8156-AC6CC3948B34",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 379}, {150, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label10",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "33CF2752-058B-406F-B303-30FE9B52977B",
+          "text" : "Air resistance",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{968, 379}, {51, 31}}",
+        "class" : "Switch",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "action" : "cswitch",
+          "frame" : "{{487, 369}, {51, 31}}",
+          "value" : true,
+          "class" : "Switch",
+          "name" : "airsw",
+          "uuid" : "98167A1F-D289-4B59-9813-56F2E5853875"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 419}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label11",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "A63D3CC8-29CD-403E-AD3D-70EF29503819",
+          "text" : "k",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{802, 418}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "E6FF9888-E764-4163-8F70-1B9C1982D31F",
+          "frame" : "{{412, 368}, {200, 32}}",
+          "custom_attributes" : "{'action':tfee}",
+          "alignment" : "right",
+          "autocorrection_type" : "no",
+          "alpha" : 1,
+          "text" : "0",
+          "font_name" : "<System>",
+          "spellchecking_type" : "no",
+          "class" : "TextField",
+          "name" : "resk",
+          "font_size" : 17
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 459}, {300, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label12",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "DD40E7E4-C267-465B-BCF4-8243FAF79130",
+          "text" : "Standard sets",
+          "alignment" : "center",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 499}, {75, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "C2C9BC29-B5D3-45EA-BFE4-14CBC546770E",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "1",
+          "class" : "Button",
+          "name" : "ss1",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{799, 499}, {75, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "D05936C0-77A3-4F48-8BCC-EE733E65A7F7",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "2",
+          "class" : "Button",
+          "name" : "ss2",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{874, 499}, {75, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "A6DB1FF8-CB5F-42D4-9549-A6972E2D7B91",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "3",
+          "class" : "Button",
+          "name" : "ss3",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{949, 499}, {75, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "uuid" : "F0010809-0550-4A4A-A69E-ACBD2B0B7C48",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "4",
+          "class" : "Button",
+          "name" : "ss4",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{725, 539}, {300, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "action" : "startsim",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "Start",
+          "uuid" : "2FE40ECE-BC0C-475F-9614-25CBDAB25E91",
+          "class" : "Button",
+          "name" : "button6",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{725, 579}, {300, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "action" : "pausesim",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "Pause",
+          "uuid" : "CD5199B7-687B-45D9-A976-D1C680FA1BFE",
+          "class" : "Button",
+          "name" : "button7",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 619}, {300, 32}}",
+        "class" : "Button",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "action" : "resetsim",
+          "frame" : "{{472, 368}, {80, 32}}",
+          "title" : "Reset",
+          "uuid" : "082489B8-A398-4835-B970-03118DCEDBE7",
+          "class" : "Button",
+          "name" : "button8",
+          "font_size" : 15
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{724, 659}, {70, 32}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "label13",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "7A540C42-700B-4C29-AA47-311219813FC2",
+          "text" : "Npart",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 18,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : true,
+        "frame" : "{{802, 659}, {222, 32}}",
+        "class" : "TextField",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "font_size" : 17,
+          "name" : "npart",
+          "frame" : "{{412, 368}, {200, 32}}",
+          "spellchecking_type" : "no",
+          "custom_attributes" : "{'action':tfee}",
+          "class" : "TextField",
+          "uuid" : "46B7BAA5-5A01-4297-AB14-B88C501E93B4",
+          "alignment" : "right",
+          "text" : "250",
+          "autocorrection_type" : "no",
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{854, 688}, {170, 16}}",
+        "class" : "Label",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "name" : "timelabel",
+          "frame" : "{{437, 368}, {150, 32}}",
+          "uuid" : "2D866848-E7CD-4627-9AED-9BB012EE1067",
+          "text" : "T = 0",
+          "alignment" : "left",
+          "class" : "Label",
+          "font_size" : 10,
+          "font_name" : "<System>"
+        }
+      },
+      {
+        "selected" : false,
+        "frame" : "{{968, 261}, {51, 31}}",
+        "class" : "Switch",
+        "nodes" : [
+
+        ],
+        "attributes" : {
+          "frame" : "{{487, 355}, {51, 31}}",
+          "value" : false,
+          "class" : "Switch",
+          "name" : "lolsw",
+          "uuid" : "1F6467BE-0A8D-4041-B4AD-664E1DFE492C"
+        }
+      }
+    ],
+    "attributes" : {
+      "border_color" : "RGBA(0.000000,0.000000,0.000000,1.000000)",
+      "enabled" : true,
+      "background_color" : "RGBA(1.000000,1.000000,1.000000,1.000000)",
+      "tint_color" : "RGBA(0.000000,0.478000,1.000000,1.000000)",
+      "name" : "xplodrrr",
+      "flex" : ""
+    }
+  }
+]
